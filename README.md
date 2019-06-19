d436376af62cd6787142b40dbae67958ee82ad34
jean philippe caillet <jeanphilippecaillet@MacBook-Pro-de-jean.local>
2019-06-19 14:16:23 +0200

    test image

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # mds_m1_Caillet_jean-Philippe_optimperf
+
+
+![alt text](https://www.linode.com/docs/databases/mongodb/build-database-clusters-with-mongodb/mongodb-cluster-diagram.png)
--- app/db.js
+++ app/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-
-mongoose.connect('mongodb+srv://user:psw@cluster0-829wl.mongodb.net/opti?retryWrites=true&w=majority', { useNewUrlParser: true })
+let url = 'mongodb+srv://user:psw@cluster0-829wl.mongodb.net/opti?retryWrites=true&w=majority'
+mongoose.connect(url, { useNewUrlParser: true })
 this.db = mongoose.connection
 module.exports = this.db
